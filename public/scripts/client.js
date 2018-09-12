eaebc5ff5cf824f4b93f8889327beb3766bb231a
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-09-12 14:05:47 -0500

    food array added to dom

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,14 @@
 console.log('client loaded');
+
+let myApp = angular.module('FoodApp', []);
+
+myApp.controller('FoodController', function() {
+    let self = this;
+    self.foods = [
+        'Pasta',
+        'Pizza',
+        'Sandwitches',
+        'Natural Flavors',
+        'Xanthan Gum'
+    ];
+});
